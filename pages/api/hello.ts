4c8c783c39fd88759177156284582477a57ae5a1
huascar-one <huascar360@gmail.com>
2022-05-17 15:25:41 -0400

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
+  estado: number;
+};
+
+function random(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ estado: random(0, 255) });
 }
